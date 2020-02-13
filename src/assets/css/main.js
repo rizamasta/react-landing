1c64f832c82db3734debcd252e1b14d265d7a051
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-13 16:38:21 +0700

    create main styles

---
+++ src/assets/css/main.js
@@ -0,0 +1,11 @@
+import grey from '@material-ui/core/colors/grey';
+
+
+
+export const BUTTON_OVAL = { borderRadius: 20, width: 90, height: 40, border: "1px solid", borderColor: grey[500], textTransform: 'capitalize' }
+
+export const DARK = {
+    color: "white",
+    backgroundColor: grey[800],
+    borderColor: grey[800]
+}
