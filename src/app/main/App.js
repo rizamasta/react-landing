f874c9d12e7eb95dc2644d6dd05926035dbb5c3b
Riza Masta Sputra <rizamasta@gmail.com>
2020-02-13 16:38:34 +0700

    update appjs

--- src/app/main/App.js
+++ src/app/main/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import { Default, EmptyLayout } from '../layouts'
+
+const options = {
+  position: 'top center',
+  timeout: 5000,
+  offset: '30px',
+  transition: 'scale'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <BrowserRouter>
+        <Switch>
+          <Route exec path="/signin" component={EmptyLayout} />
+          <Route exec path="/signup" component={EmptyLayout} />
+          <Route exec path="/" component={Default} />
+        </Switch>
+      </BrowserRouter>
+    </AlertProvider>
   );
 }
 
